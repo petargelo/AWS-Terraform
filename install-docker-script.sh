60a2a0798cd4af72aaedea2fccc983b5353952f5
Petar Gelo <petar.gelo@asseco-see.hr>
2024-03-19 16:18:30 +0100

    Add intall-docker-script.sh that stores script to install and start docker and create testcontainer

---
+++ install-docker-script.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo dnf update -y 
+sudo dnf install -y docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+newgrp docker
+docker run --name testcontainer -p 8080:80 -d nginx 
